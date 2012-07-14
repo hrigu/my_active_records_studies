5791779241c590bdf47b4988c9c25270d6188fec
chrigu <chmuehlethaler@gmail.com>
2012-07-14 14:32:33 +0200

    made a real spec out of graph

--- spec/models/graph_spec.rb
+++ spec/models/graph_spec.rb
@@ -12,9 +12,12 @@ describe "playing with a graph" do
     fb = FactoryGirl.create(:client, name: "fb", users: [chrigu, dominic])
     substring = FactoryGirl.create(:client, name: "substring", users: [xa])
 
-    staff_chrigu = FactoryGirl.create(:staff, abbreviation: "muc", client: planik)
-    staff_xa = FactoryGirl.create(:staff, abbreviation: "xa", client: planik)
-    staff_dominic = FactoryGirl.create(:staff, abbreviation: "dom", client: planik)
+    staff_xa_planik = FactoryGirl.create(:staff, abbreviation: "muc", client: planik)
+    staff_xa_planik = FactoryGirl.create(:staff, abbreviation: "xa", client: planik)
+    staff_dominic_planik = FactoryGirl.create(:staff, abbreviation: "dom", client: planik)
+
+    staff_dominic_fb = FactoryGirl.create(:staff, abbreviation: "dom_fb", client: fb)
+
 
     yellow = FactoryGirl.create(:color)
     red = FactoryGirl.create(:color, name: "red")
@@ -24,13 +27,20 @@ describe "playing with a graph" do
     mittelschicht = FactoryGirl.create(:service_type, explanation: "mittelschicht", color: red)
     spaetschicht = FactoryGirl.create(:service_type, explanation: "spaetschicht", color: blue)
 
-    service1 = FactoryGirl.create(:service, service_type: fruehschicht, staff: staff_chrigu)
+    service1 = FactoryGirl.create(:service, service_type: fruehschicht, staff: staff_xa_planik)
+    service1 = FactoryGirl.create(:service, service_type: spaetschicht, staff: staff_dominic_fb)
 
+    #ActiveRecord::Base.logger = Logger.new(STDOUT)
     found_yellow = Color.find_by_name "yellow"
+    service_type = found_yellow.service_types[0]
+    service_type.explanation.should match "fruehschicht"
+    chrigu = service_type.services[0].staff.client.users[0]
+    chrigu.email.should match "chrigu@planik.ch"
+    fb = chrigu.clients[1]
+    fb.name.should match "fb"
+    service_type = fb.staffs[0].services[0].service_type
+    service_type.explanation.should match "spaetschicht"
 
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-    found_user = found_yellow.service_types[0].services[0].staff.client.users[0]
-    puts found_user.email
 
 
     #chrigu.clients.size.should == 2
