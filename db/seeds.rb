b937b9d957e36837957bb3a8630f6f9d06a1f2b2
chrigu <chmuehlethaler@gmail.com>
2012-07-17 22:14:21 +0200

    two service types

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def to_date_time time
+  DateTime.strptime(time, '%H:%M')
+end
 
-myColors = [
+
+my_colors = [
     {name: 'white',  rgb: "C0C0C0", sort_order: 0},
     {name: 'silver', rgb: "FFFFFF", sort_order: 1},
     {name: 'red', rgb: "FF0000", sort_order: 2},
@@ -16,5 +20,14 @@ myColors = [
     {name: 'purple', rgb: "800080", sort_order: 5},
 ]
 
-Color.create myColors
+Color.create my_colors
+
+my_service_types = [
+    {time_from: to_date_time('08:00'), time_to: to_date_time('12:00'), duration: to_date_time('4:00'), explanation: "Fruehschicht", color: Color.find_by_name!('red')},
+    {time_from: to_date_time('12:00'), time_to: to_date_time('16:00'), duration: to_date_time('4:00'), explanation: "Mittelschicht", color: Color.find_by_name('lime')},
+]
+
+
+ServiceType.create my_service_types
+
 AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
